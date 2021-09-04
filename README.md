7feb720a8f6bb2ff168f56f2119743ab90f077b0
Robin Sallis <Toutouwai@users.noreply.github.com>
2021-09-04 21:29:24 +1200

    v0.1.0

---
+++ README.md
@@ -0,0 +1,37 @@
+# Repeater Easy Sort
+
+Adds a compact "easy sort" mode to Repeater and Repeater Matrix, making those fields easier to sort when there are a large number of items.
+
+The module also enhances Repeater Matrix by allowing a colour to be set for each matrix type. This colour is used in the item headers and in the "add" links, to help visually distinguish different matrix types in the inputfield.
+
+## Easy-sort mode
+
+Each Repeater/Matrix item gets an double-arrow icon in the item header. Click this icon to enter easy-sort mode.
+
+While in easy-sort mode:
+* The items will reduce in width so that more items can be shown on the screen at once. The minimum width is configurable in the field settings.
+* Any items that were in an open state are collapsed, but when you exit easy-sort mode the previously open items will be reopened.
+* You can drag an item left/right/up/down to sort it within the items. 
+* The item that you clicked the icon for is shown with a black background. This makes it easier to find the item you want to move in easy-sort mode.
+* You can click an item header to open the item.
+* An "Exit easy-sort mode" button appears at the bottom of the inputfield.
+
+## Configuration
+
+In the field settings for Repeater and Repeater Matrix fields you can define a minimum width in pixels for items in easy-sort mode. While in easy-sort mode the items will be sized to neatly fill the available width on any screen size but will never be narrower than the width you set here.
+
+In the field settings for Repeater Matrix you can define a custom header colour for each matrix type using an HTML "color" type input. The default colour for this type of input is black, so when black is selected in the input it means that no custom colour will be applied to the header.
+
+## Screencasts
+
+####A Repeater field
+
+![res-1](https://user-images.githubusercontent.com/1538852/132089653-1e56cebe-411a-44c5-8a32-ce730634c9ed.gif)
+
+####A Repeater Matrix field with custom header colours
+
+![res-2](https://user-images.githubusercontent.com/1538852/132089655-01e02396-b095-455b-ba0c-dcd03dfcfccb.gif)
+
+## Exclusions
+
+The easy sort mode is only possible on Repeater/Matrix fields that do not use the "item depth" option.
---
+++ RepeaterEasySort.js
@@ -0,0 +1,85 @@
+(function($) {
+
+	var rs_config = ProcessWire.config.RepeaterEasySort;
+
+	function addSortIcon() {
+		$('.RepeaterEasySort .InputfieldRepeaterItemControls').each(function() {
+			if(!$(this).children('.easy-sort').length) {
+				$(this).children('.toggle-icon').before('<i class="fa fa-arrows-v easy-sort" title="' + rs_config.labels.sort + '"></i>');
+			}
+		});
+	}
+
+	$(document).ready(function() {
+
+		// Add sort icons on DOM ready
+		addSortIcon();
+
+		// Add sort icons when Repeater inputfield reloaded
+		$(document).on('reloaded', '.InputfieldRepeater', function() {
+			setTimeout(addSortIcon, 100);
+		});
+
+		// When easy-sort icon clicked
+		$(document).on('click', '.InputfieldRepeaterItemControls .easy-sort', function(event) {
+			event.stopPropagation();
+			var $item = $(this).closest('.InputfieldRepeaterItem');
+			var $inputfield = $(this).closest('.InputfieldRepeater');
+			var $content = $inputfield.children('.InputfieldContent');
+
+			var min_width = $inputfield.data('res-min-width');
+			var available_width = $content.width();
+			var divisor = Math.floor(available_width / min_width);
+			if(divisor > 8) divisor = 8;
+			$inputfield.addClass('width-1-' + divisor);
+
+			// Add exit button
+			$content.children('.InputfieldRepeaterAddItem').after('<button type="button" class="easy-sort-exit ui-button ui-priority-secondary">' + rs_config.labels.exit + '</button>');
+
+			// Add focus class
+			$item.addClass('easy-sort-focus');
+
+			// Add sort-active class
+			$inputfield.addClass('easy-sort-active');
+
+			// Close any open items
+			var $items = $content.children('.Inputfields').children('.InputfieldRepeaterItem');
+			$items.filter(':not(.InputfieldStateCollapsed)').each(function() {
+				$(this).addClass('res-was-open').children('.InputfieldHeader').find('.toggle-icon').click();
+			});
+
+			// Two-dimensional sorting
+			$inputfield.find('.Inputfields:eq(0)').each(function() {
+				$(this).sortable('option', 'axis', false);
+			});
+		});
+
+		// When exit button clicked
+		$(document).on('click', '.easy-sort-exit', function(event) {
+			var $inputfield = $(this).closest('.InputfieldRepeater');
+
+			// Remove button
+			$(this).remove();
+
+			// Remove sort-active class
+			$inputfield.removeClass('easy-sort-active');
+
+			// One-dimensional sorting
+			$inputfield.find('.Inputfields:eq(0)').each(function() {
+				$(this).sortable('option', 'axis', 'y');
+			});
+
+			var $items = $inputfield.children('.InputfieldContent').children('.Inputfields').children('.InputfieldRepeaterItem');
+			// Remove width class
+			$inputfield.removeClass('width-1-1 width-1-2 width-1-3 width-1-4 width-1-5 width-1-6 width-1-7 width-1-8');
+			// Remove sort-active class
+			$items.filter('.easy-sort-focus').removeClass('easy-sort-focus');
+			// Open any previously open items
+			$items.filter('.res-was-open').each(function() {
+				$(this).addClass('res-was-open').children('.InputfieldHeader').find('.toggle-icon').click();
+			});
+		});
+
+	});
+
+}(jQuery));
