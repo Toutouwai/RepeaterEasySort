e21ebc6efeb3ecdeba7b64382eb2a49f0729ff4d
Robin Sallis <Toutouwai@users.noreply.github.com>
2022-02-19 14:49:07 +1300

    v0.2.0
    Adds mode buttons to the field header. Adds a config option to enable easy-sort mode for a field by default.

--- README.md
+++ README.md
@@ -16,13 +16,18 @@ The module also enhances Repeater Matrix by allowing a colour to be set for each
 
 ## Easy-sort mode
 
-Each Repeater/Matrix item gets an double-arrow icon in the item header. Click this icon to enter easy-sort mode.
+There are two ways to enter easy-sort mode.
+
+1\. Click the double-arrow in a Repeater item header. This activates easy-sort mode and also highlights the item with a black background so it's easier to find it in easy-sort mode.
+
+2\. Click the horizontal ellipsis icon in the Repeater field header to activate easy-sort mode. To return to normal mode click the vertical ellipsis icon.
+
+![Mode buttons](https://user-images.githubusercontent.com/1538852/154781102-af145c2b-51aa-43ed-aa91-08fcc533d8c8.png)
 
 While in easy-sort mode:
 * The items will reduce in width so that more items can be shown on the screen at once. The minimum width is configurable in the field settings.
 * Any items that were in an open state are collapsed, but when you exit easy-sort mode the previously open items will be reopened.
-* You can drag an item left/right/up/down to sort it within the items. 
-* The item that you clicked the icon for is shown with a black background. This makes it easier to find the item you want to move in easy-sort mode.
+* You can drag an item left/right/up/down to sort it within the items.
 * You can click an item header to open the item.
 * An "Exit easy-sort mode" button appears at the bottom of the inputfield.
 
@@ -30,7 +35,13 @@ While in easy-sort mode:
 
 In the field settings for Repeater and Repeater Matrix fields you can define a minimum width in pixels for items in easy-sort mode. While in easy-sort mode the items will be sized to neatly fill the available width on any screen size but will never be narrower than the width you set here.
 
+If desired you can enable easy-sort mode for a field by default. Since easy-sort is then the default mode for the field no "Exit easy-sort mode" button is shown. Use the mode buttons in the field header to change between easy-sort and normal mode when needed.
+
+![Config](https://user-images.githubusercontent.com/1538852/154781103-85eb2832-e0c9-492b-a7ce-dd4f1f55cf14.png)
+
 In the field settings for Repeater Matrix you can define a custom header colour for each matrix type using an HTML "color" type input. The default colour for this type of input is black, so when black is selected in the input it means that no custom colour will be applied to the header.
+
+![Matrix header colour](https://user-images.githubusercontent.com/1538852/154781183-434c66aa-5dd5-469e-a756-d3ad75cd27a0.png)
 
 ## Exclusions
 
--- RepeaterEasySort.js
+++ RepeaterEasySort.js
@@ -1,84 +1,110 @@
 (function($) {
 
-	var rs_config = ProcessWire.config.RepeaterEasySort;
+	var res_config = ProcessWire.config.RepeaterEasySort;
 
-	function addSortIcon() {
-		$('.RepeaterEasySort .InputfieldRepeaterItemControls').each(function() {
-			if(!$(this).children('.easy-sort').length) {
-				$(this).children('.toggle-icon').before('<i class="fa fa-arrows-v easy-sort" title="' + rs_config.labels.sort + '"></i>');
+	function addEasySortControls() {
+		$('.RepeaterEasySort').each(function() {
+			if(!$(this).find('> .InputfieldHeader > .easy-sort-mode').length) {
+				$(this).find('> .InputfieldHeader').append('<div class="easy-sort-mode"><i class="fa fa-ellipsis-h res-easy-sort-mode" title="' + res_config.labels.sort_mode + '"></i><i class="fa fa-ellipsis-v res-normal-mode" title="' + res_config.labels.normal_mode + '"></i></div>');
 			}
+			$(this).find('.InputfieldRepeaterItemControls').each(function() {
+				if(!$(this).children('.easy-sort').length) {
+					$(this).children('.toggle-icon').before('<i class="fa fa-arrows-v easy-sort" title="' + res_config.labels.sort + '"></i>');
+				}
+			});
+			// Enter easy-sort mode if enabled by default
+			if($(this).hasClass('res-active-default') && !$(this).hasClass('res-active-default-init')) {
+				$(this).find('> .InputfieldHeader > .easy-sort-mode > .res-easy-sort-mode').click();
+				$(this).addClass('res-active-default-init');
+			}
+		});
+	}
+
+	function startEasySort($inputfield, $item) {
+		var $content = $inputfield.children('.InputfieldContent');
+
+		var min_width = $inputfield.data('res-min-width');
+		var available_width = $content.width();
+		var divisor = Math.floor(available_width / min_width);
+		if(divisor > 8) divisor = 8;
+		$inputfield.addClass('width-1-' + divisor);
+
+		// Add exit button
+		$content.children('.InputfieldRepeaterAddItem').after('<button type="button" class="easy-sort-exit ui-button ui-state-default ui-priority-secondary"><span class="ui-button-text"><i class="fa fa-ellipsis-v"></i> ' + res_config.labels.exit + '</span></button>');
+
+		// Add focus class
+		if($item) $item.addClass('easy-sort-focus');
+
+		// Add sort-active class
+		$inputfield.addClass('easy-sort-active');
+
+		// Close any open items
+		var $items = $content.find('.Inputfields > .InputfieldRepeaterItem');
+		$items.filter(':not(.InputfieldStateCollapsed)').each(function() {
+			$(this).addClass('res-was-open').children('.InputfieldHeader').find('.toggle-icon').click();
+		});
+
+		// Two-dimensional sorting
+		$inputfield.find('> .InputfieldContent > .Inputfields:eq(0)').sortable('option', 'axis', false);
+	}
+
+	function endEasySort($inputfield) {
+		// Remove button
+		$inputfield.find('> .InputfieldContent > .easy-sort-exit').remove();
+
+		// Remove sort-active class
+		$inputfield.removeClass('easy-sort-active');
+
+		// One-dimensional sorting
+		$inputfield.find('> .InputfieldContent > .Inputfields:eq(0)').sortable('option', 'axis', 'y');
+
+		var $items = $inputfield.find('> .InputfieldContent > .Inputfields > .InputfieldRepeaterItem');
+		// Remove width class
+		$inputfield.removeClass('width-1-1 width-1-2 width-1-3 width-1-4 width-1-5 width-1-6 width-1-7 width-1-8');
+		// Remove sort-active class
+		$items.filter('.easy-sort-focus').removeClass('easy-sort-focus');
+		// Open any previously open items
+		$items.filter('.res-was-open').each(function() {
+			$(this).addClass('res-was-open').children('.InputfieldHeader').find('.toggle-icon').click();
 		});
 	}
 
 	$(document).ready(function() {
 
-		// Add sort icons on DOM ready
-		addSortIcon();
-
-		// Add sort icons when Repeater inputfield reloaded
+		// When Repeater inputfield reloaded
 		$(document).on('reloaded', '.InputfieldRepeater', function() {
-			setTimeout(addSortIcon, 100);
+			// Add easy-sort controls
+			setTimeout(addEasySortControls, 100);
 		});
 
-		// When easy-sort icon clicked
+		// When easy-sort mode button clicked
+		$(document).on('click', '.res-easy-sort-mode', function(event) {
+			event.stopPropagation();
+			var $inputfield = $(this).closest('.InputfieldRepeater');
+			// Return early if easy-sort already active
+			if($inputfield.hasClass('easy-sort-active')) return;
+			startEasySort($inputfield, null);
+		});
+
+		// When normal mode button or exit button clicked
+		$(document).on('click', '.res-normal-mode, .easy-sort-exit', function(event) {
+			event.stopPropagation();
+			var $inputfield = $(this).closest('.InputfieldRepeater');
+			// Return early if easy-sort not active
+			if(!$inputfield.hasClass('easy-sort-active')) return;
+			endEasySort($inputfield);
+		});
+
+		// When item easy-sort button clicked
 		$(document).on('click', '.InputfieldRepeaterItemControls .easy-sort', function(event) {
 			event.stopPropagation();
 			var $item = $(this).closest('.InputfieldRepeaterItem');
 			var $inputfield = $(this).closest('.InputfieldRepeater');
-			var $content = $inputfield.children('.InputfieldContent');
-
-			var min_width = $inputfield.data('res-min-width');
-			var available_width = $content.width();
-			var divisor = Math.floor(available_width / min_width);
-			if(divisor > 8) divisor = 8;
-			$inputfield.addClass('width-1-' + divisor);
-
-			// Add exit button
-			$content.children('.InputfieldRepeaterAddItem').after('<button type="button" class="easy-sort-exit ui-button ui-state-default ui-priority-secondary"><span class="ui-button-text">' + rs_config.labels.exit + '</span></button>');
-
-			// Add focus class
-			$item.addClass('easy-sort-focus');
-
-			// Add sort-active class
-			$inputfield.addClass('easy-sort-active');
-
-			// Close any open items
-			var $items = $content.children('.Inputfields').children('.InputfieldRepeaterItem');
-			$items.filter(':not(.InputfieldStateCollapsed)').each(function() {
-				$(this).addClass('res-was-open').children('.InputfieldHeader').find('.toggle-icon').click();
-			});
-
-			// Two-dimensional sorting
-			$inputfield.find('.Inputfields:eq(0)').each(function() {
-				$(this).sortable('option', 'axis', false);
-			});
+			startEasySort($inputfield, $item);
 		});
 
-		// When exit button clicked
-		$(document).on('click', '.easy-sort-exit', function(event) {
-			var $inputfield = $(this).closest('.InputfieldRepeater');
-
-			// Remove button
-			$(this).remove();
-
-			// Remove sort-active class
-			$inputfield.removeClass('easy-sort-active');
-
-			// One-dimensional sorting
-			$inputfield.find('.Inputfields:eq(0)').each(function() {
-				$(this).sortable('option', 'axis', 'y');
-			});
-
-			var $items = $inputfield.children('.InputfieldContent').children('.Inputfields').children('.InputfieldRepeaterItem');
-			// Remove width class
-			$inputfield.removeClass('width-1-1 width-1-2 width-1-3 width-1-4 width-1-5 width-1-6 width-1-7 width-1-8');
-			// Remove sort-active class
-			$items.filter('.easy-sort-focus').removeClass('easy-sort-focus');
-			// Open any previously open items
-			$items.filter('.res-was-open').each(function() {
-				$(this).addClass('res-was-open').children('.InputfieldHeader').find('.toggle-icon').click();
-			});
-		});
+		// Add easy-sort controls on DOM ready
+		addEasySortControls();
 
 	});
 
